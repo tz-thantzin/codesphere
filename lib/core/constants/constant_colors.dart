c280e170416fdf73973e1e9d89e2547192a4aec5
thantzin <dev.thantzin@gmail.com>
2025-12-01 09:17:53 +0700

    Initial Flutter Project Stepup

---
+++ lib/core/constants/constant_colors.dart
@@ -0,0 +1,6 @@
+import 'dart:ui';
+
+const Color primaryDark = Color(0xFF0D111C);
+const Color accentIndigo = Color(0xFF6366F1);
+const Color accentPurple = Color(0xFFA855F7);
+const Color textColor = Color(0xFFE5E7EB);
